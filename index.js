6117dacb41a04d907cc70930743606208281cafe
Hrithik248 <thebatrazzpurohit@gmail.com>
2023-06-04 22:23:36 +0530

    setting up wview engine

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const port=8000;
 const app= express();
 //use express router
 app.use('/',require('./routes'));
+// set up view engine
+app.set('view engine','ejs');
+app.set('views','./views');
 app.listen(port,function(err){
     if(err){
         console.log(`Error in starting Server : ${err}`);
