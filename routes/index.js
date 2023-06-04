40ac432108888a36bf311582c814e9f9e982a1d0
Hrithik248 <thebatrazzpurohit@gmail.com>
2023-06-04 22:05:39 +0530

    add users router and controller

--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,5 @@ const router= express.Router();
 console.log('router loaded');
 const home_controller=require('../controllers/home_controller');
 router.get('/',home_controller.home);
+router.use('/users',require('./users'));
 module.exports=router;
---
+++ routes/users.js
@@ -0,0 +1,5 @@
+const express= require('express');
+const router=express.Router();
+const user_controller=require('../controllers/users_controller');
+router.get('/profile',user_controller.profile);
+module.exports=router;
